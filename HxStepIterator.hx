667b07473d6ed011b737fc5fe40d961233cb35e7
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-07-16 19:48:51 +0100

    Stepped iterator util iterator

---
+++ HxStepIterator.hx
@@ -0,0 +1,14 @@
+class StepIterator {
+  var end:Int;
+  var step:Int;
+  var index:Int;
+
+  public inline function new(start:Int, end:Int, step:Int) {
+    this.index = start;
+    this.end = end;
+    this.step = step;
+  }
+
+  public inline function hasNext() return index < end;
+  public inline function next() return (index += step) - step;
+}
