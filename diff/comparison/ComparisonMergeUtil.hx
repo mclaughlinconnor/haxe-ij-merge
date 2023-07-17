11881a6dde26484fa6c8e477d25a999ec673112a
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-07-17 20:44:25 +0100

    Add missing MergeRange import to ComparisonMergeUtil

--- diff/comparison/ComparisonMergeUtil.hx
+++ diff/comparison/ComparisonMergeUtil.hx
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package diff.comparison;
 
+import diff.util.MergeRange;
 import diff.comparison.iterables.FairDiffIterable;
 import diff.util.Side;
 import config.DiffConfig;
