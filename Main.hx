6953f5eeb40c650c2e65a2814df8eefe21e9d3a2
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-07-17 23:48:57 +0100

    Add test Main file

--- Main.hx
+++ Main.hx
@@ -1,9 +1,17 @@
 /**
 	Multi-line comments for documentation.
 **/
+
+import diff.comparison.MergeResolveUtil;
+
 class Main {
 	static public function main():Void {
 		// Single line comment
 		trace("Hello World");
+
+    var merge = MergeResolveUtil.tryResolve('Hello', 'hello', 'hello world');
+    if (merge != null) {
+      trace(merge);
+    }
 	}
 }
