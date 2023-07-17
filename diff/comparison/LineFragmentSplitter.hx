fa51022cfa534b8f87205559daad3447188f4653
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-07-17 21:12:56 +0100

    LineFragmentSplitter imports

--- diff/comparison/LineFragmentSplitter.hx
+++ diff/comparison/LineFragmentSplitter.hx
@@ -1,6 +1,8 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package diff.comparison;
 
+import diff.comparison.ByWordRt;
+import diff.util.Range;
 import diff.comparison.iterables.FairDiffIterable;
 
 /*
