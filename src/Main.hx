bbeb0759a24a4fe1827548736f82b29a79771ee7
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-07-29 23:09:17 +0100

    Update expected result

--- src/Main.hx
+++ src/Main.hx
@@ -112,7 +112,7 @@ class API {
 		right = "1 ======\n2 ======\n3 ======\nnew both\n4 ======\nmodify both\n5 ======\nmodify right\n6 ======\n7 ======\nmodify\n8 ======\n";
 		middle = "1 ======\n2 ======\nremove right\n3 ======\n4 ======\nmodify\n5 ======\nmodify\n6 ======\n7 ======\ndelete modify\n8 ======\n";
 
-		var expected = "1 ======\ninsert left\n2 ======\nremove right\n3 ======\nnew both\n4 ======\nmodify both\n5 ======\nmodify\n6 ======\n7 ======\ndelete modify\n8 ======";
+		var expected = "1 ======\ninsert left\n2 ======\n3 ======\nnew both\n4 ======\nmodify both\n5 ======\nmodify right\n6 ======\n7 ======\ndelete modify\n8 ======\n";
 
 		var viewer = new MergeThreesideViewer([left, middle, right], middle);
 		viewer.rediff(false);
