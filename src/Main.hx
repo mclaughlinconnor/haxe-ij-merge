ea5b0002c4be18112ec2b5f4ae02fc7ee5e8305f
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-08-05 20:01:26 +0100

    Remove some dead code

--- src/Main.hx
+++ src/Main.hx
@@ -34,19 +34,6 @@ class API {
 		return [finalMergedText, formattedLeft, formattedMiddle, formattedRight];
 	}
 
-	static public function test():String {
-		var left = "1 ======\ninsert left\n2 ======\nremove right\n3 ======\nnew both\n4 ======\nmodify both\n5 ======\nmodify\n6 ======\n7 ======\n8 ======\n";
-		var right = "1 ======\n2 ======\n3 ======\nnew both\n4 ======\nmodify both\n5 ======\nmodify right\n6 ======\n7 ======\nmodify\n8 ======\n";
-		var middle = "1 ======\n2 ======\nremove right\n3 ======\n4 ======\nmodify\n5 ======\nmodify\n6 ======\n7 ======\ndelete modify\n8 ======\n";
-
-		var expected = "1 ======\ninsert left\n2 ======\n3 ======\nnew both\n4 ======\nmodify both\n5 ======\nmodify right\n6 ======\n7 ======\ndelete modify\n8 ======\n";
-
-		var viewer = new MergeThreesideViewer([left, middle, right], middle);
-		viewer.rediff(false);
-		trace(viewer.myModel.getDocument() == expected);
-		return viewer.myModel.getDocument();
-	}
-
 	#if js
 	static public function decorate():Void {
 		DiffDecorations.decorate();
