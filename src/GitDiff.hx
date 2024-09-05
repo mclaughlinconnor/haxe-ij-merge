ec11bdc7b8e1c1cade810acea4423245ebaab3d2
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-09-05 23:33:26 +0100

    Fix lines after last conflict being excluded from output

--- src/GitDiff.hx
+++ src/GitDiff.hx
@@ -20,21 +20,24 @@ class GitDiff {
 
 	public function formatDiff():String {
 		var formattedString = new StringBuf();
-    var baseSide = ThreeSide.fromEnum(ThreeSideEnum.BASE);
+		var baseSide = ThreeSide.fromEnum(ThreeSideEnum.BASE);
+		var baseLines = myLines[baseSide.getIndex()];
 
 		for (change in myChanges) {
-      var changeStartLine = change.getStartLineA();
+			var changeStartLine = change.getStartLineA();
 
-      if (currentLine < changeStartLine) {
-        currentLine = pushLinesUntil(formattedString, myLines[baseSide.getIndex()], currentLine, changeStartLine);
-      }
+			if (currentLine < changeStartLine) {
+				currentLine = pushLinesUntil(formattedString, baseLines, currentLine, changeStartLine);
+			}
 
 			if (change.isResolvedA()) {
-        currentLine = pushLinesUntil(formattedString, myLines[baseSide.getIndex()], changeStartLine, change.getEndLineA());
+				currentLine = pushLinesUntil(formattedString, baseLines, changeStartLine, change.getEndLineA());
 			} else {
 				formattedString.add(createConflictHunk(change));
-      }
+			}
 		}
+
+		currentLine = pushLinesUntil(formattedString, baseLines, currentLine, baseLines.length, false);
 
 		return formattedString.toString();
 	}
@@ -43,7 +46,7 @@ class GitDiff {
 		var hunk = new StringBuf();
 		hunk.add(LEFT_MARKER);
 
-    var sideEnum = ThreeSideEnum.LEFT;
+		var sideEnum = ThreeSideEnum.LEFT;
 		var side = ThreeSide.fromEnum(sideEnum);
 		var start = change.getStartLineB(sideEnum);
 		var end = change.getEndLineB(side);
@@ -52,7 +55,7 @@ class GitDiff {
 
 		hunk.add(PRE_BASE_MARKER);
 
-    sideEnum = ThreeSideEnum.BASE;
+		sideEnum = ThreeSideEnum.BASE;
 		side = ThreeSide.fromEnum(sideEnum);
 		start = change.getStartLineB(sideEnum);
 		end = change.getEndLineB(side);
@@ -61,7 +64,7 @@ class GitDiff {
 
 		hunk.add(POST_BASE_MARKER);
 
-    sideEnum = ThreeSideEnum.RIGHT;
+		sideEnum = ThreeSideEnum.RIGHT;
 		side = ThreeSide.fromEnum(sideEnum);
 		start = change.getStartLineB(sideEnum);
 		end = change.getEndLineB(side);
--- src/util/PushLineUntil.hx
+++ src/util/PushLineUntil.hx
@@ -1,11 +1,15 @@
 package util;
 
-function pushLinesUntil(formattedString:StringBuf, lines:Array<String>, start: Int, end:Int) {
-  while (start < end) {
-    formattedString.add(lines[start]);
-    formattedString.add("\n");
-    start++;
-  }
+function pushLinesUntil(formattedString:StringBuf, lines:Array<String>, start:Int, end:Int, trailingNewline:Bool = true) {
+	while (start < end - 1) {
+		formattedString.add(lines[start++]);
+		formattedString.add("\n");
+	}
 
-  return start;
+	formattedString.add(lines[start]);
+	if (trailingNewline) {
+		formattedString.add("\n");
+	}
+
+	return start;
 }
