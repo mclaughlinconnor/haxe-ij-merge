dbd39bedc5a1b3c21fb5313b20221714984209d2
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-09-06 20:03:24 +0100

    Fix pushlineuntil having extra lines

--- src/util/PushLineUntil.hx
+++ src/util/PushLineUntil.hx
@@ -1,12 +1,16 @@
 package util;
 
 function pushLinesUntil(formattedString:StringBuf, lines:Array<String>, start:Int, end:Int, trailingNewline:Bool = true) {
+	if (start == end) {
+		return start;
+	}
+
 	while (start < end - 1) {
 		formattedString.add(lines[start++]);
 		formattedString.add("\n");
 	}
 
-	formattedString.add(lines[start]);
+	formattedString.add(lines[start++]);
 	if (trailingNewline) {
 		formattedString.add("\n");
 	}
